2a93aa969cd740c93f4f37ab3a6a83609ca7a96f
Hyp4r <fkatholm@gmail.com>
2022-10-07 23:32:16 +0200

    ded

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,7 +9,7 @@ const commands = [
 ]
 	.map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new rest({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
