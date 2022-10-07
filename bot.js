6e4c74d3887863cf84971ca9a34bcd8ed57b1815
Hyp4r <fkatholm@gmail.com>
2022-10-07 19:30:39 +0200

    Update bot.js
    Fixed command Hello

--- bot.js
+++ bot.js
@@ -5,9 +5,13 @@ const commands = [
     name: 'ping',
     description: 'Replies with Pong!',
   },
+  {
+    name: 'Hello',
+    description: 'Replies with Hello!',
+  },
 ];
 
-const rest = new REST({ version: '10' }).setToken('token');
+const rest = new REST({ version: '10' }).setToken('9ad83a292cf747bcb6b47c12e42d5732754b794918376e5686981b144f0b9d87');
 
 (async () => {
   try {
