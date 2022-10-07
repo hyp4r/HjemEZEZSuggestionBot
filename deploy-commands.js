d18d69a121f967bf77d913465d33dfe2712ab275
Hyp4r <fkatholm@gmail.com>
2022-10-07 23:13:19 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,7 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('Hello').setDescription('Replies with Hello!')
 ]
 	.map(command => command.toJSON());
 
