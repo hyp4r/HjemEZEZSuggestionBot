deddec5be1b6c0221cf43183530b3edf4773a46f
Hyp4r <fkatholm@gmail.com>
2022-10-08 20:04:03 +0200

    so much shit

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
+const fs = require('node:fs');
+const path = require('node:path');
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits, InteractionResponse } = require('discord.js');
+require('discord.js');
+const { Client, GatewayIntentBits, InteractionResponse, Collection, TextChannel } = require('discord.js');
 const { token } = require('./config.json');
+const { channel } = require('node:diagnostics_channel');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', c => {
@@ -13,25 +29,17 @@ client.once('ready', c => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-  console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered command /${interaction.commandName}.\n`);
-	const { commandName } = interaction;
+	const command = interaction.client.commands.get(interaction.commandName);
 
+	if (!command) return;
 
-  switch(commandName) {
-    case 'ping':
-      await interaction.reply('Pong!');
-      break;
-    case 'server':
-      await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreation Date: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
-      break;
-    case 'user':
-      await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-      break;
-    case 'hello':
-      await interaction.reply('Hello!');
-      break;
-};
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-// Login to Discord with your client's token
+
 client.login(token);
--- commands/forslag.js
+++ commands/forslag.js
@@ -1,14 +1,42 @@
-const { SlashCommandBuilder } = require('discord.js');
+// require('discord.js');
+// const { Client, SlashCommandBuilder, Message } = require('discord.js');
+// const hchannel = client.channels.cache.get("222086648706498562");
+
+// module.exports = {
+// 	data: new SlashCommandBuilder()
+//         .setName('forslag')
+//         .setDescription('Skriv et forslag')
+//         .addStringOption(option =>
+// 			option.setName('input')
+// 				.setDescription('Selve forslaget')
+// 				.setRequired(true)),
+// 	async execute(interaction) {
+// 		await hchannel.send('Hello!');
+// 	},
+// };
+
+const { EmbedBuilder, SlashCommandBuilder, Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 module.exports = {
-	data: new SlashCommandBuilder()
-        .setName('forslag')
-        .setDescription('Skriv et forslag')
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input!')
         .addStringOption(option =>
-			option.setName('input')
-				.setDescription('Selve forslaget')
-				.setRequired(true)),
-	async execute(interaction) {
-		await interaction.reply('Hello!');
-	},
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true)),
+    async execute(interaction) {
+        const input = await interaction.options.getString('input');
+        // await interaction.reply('Hello');
+        const channel = await interaction.client.channels.cache.get('1028347318136733797').send(`${input}`);
+        console.log(channel.id);
+    },
 };
