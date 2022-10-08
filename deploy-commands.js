384c22e8b8fbdc9a9b67dfc6d5d245636f9bb235
Hyp4r <fkatholm@gmail.com>
2022-10-08 14:11:27 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,16 +1,16 @@
 const { REST, SlashCommandBuilder, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-	new SlashCommandBuilder().setName('hello').setDescription('Replies with Hello!'),
-]
-	.map(command => command.toJSON());
+ const commands = [
+ 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+ 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+ 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+ 	new SlashCommandBuilder().setName('hello').setDescription('Replies with Hello!'),
+ ]
+ 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+ 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+ 	.catch(console.error);
