3efbc48fb61cad002c9c9543baf03f48063b8913
Hyp4r <fkatholm@gmail.com>
2022-10-08 23:26:42 +0200

    Forslag command
    /forslag virker nu (kinda) og har en tæller

--- commands/forslag.js
+++ commands/forslag.js
@@ -1,4 +1,11 @@
-const { EmbedBuilder, SlashCommandBuilder, Client, GatewayIntentBits } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, Client, GatewayIntentBits, messageLink } = require('discord.js');
+// const { Suggestion_nr } = require('../config.json')
+var { Suggestion_nr } = require('./sg_num.json')
+
+const fs = require('fs');
+const path = require('path');
+
+console.log(Suggestion_nr);
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -13,25 +20,35 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('forslag')
         .setDescription('Skriv et forslag')
+		.addStringOption(option =>
+			option.setName('valg')
+				.setDescription('Vælg et forslag')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Map', value: 'Lav et forslag til maps'},
+					{ name: 'Discord', value: 'Lav et forslag til Discorden'},
+					{ name: 'Event', value: 'Lav et forslag til events'},
+				))
         .addStringOption(option =>
             option.setName('input')
                 .setDescription('Forslaget')
                 .setRequired(true)),
     async execute(interaction) {
-		message.interaction.client.channels.cache.get.send("https://cdn.discordapp.com/avatars/"+message.author.id+"/"+message.author.avatar+".jpeg");
         const input = await interaction.options.getString('input');
 		const exampleEmbed = new EmbedBuilder()
 			.setColor(0x0099FF)
-			.setAuthor()
-			.setImage(avatarURL)
-			.setTitle('')
+			.setTitle('he')
 			.addFields(
-			{ name: '', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
+			{ name: `Suggestion #${Suggestion_nr}`, value: `${input}`, inline: false },
 	)
-	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-		await interaction.client.channels.cache.get('1028347318136733797').send({ embeds: [exampleEmbed] });
+		const channel = await interaction.client.channels.cache.get('1028347318136733797').send({ embeds: [exampleEmbed] });
+		await interaction.reply('Dit forslag er blevet indsendt');
+		Suggestion_nr = Suggestion_nr + 1;
+		console.log(Suggestion_nr);
+		const Sg_nr = {
+			"Suggestion_nr": Suggestion_nr
+		}
+		 
+		fs.writeFileSync(path.resolve(__dirname, 'sg_num.json'), JSON.stringify(Sg_nr));
     },
 };
