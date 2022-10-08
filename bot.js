77d34f8cdcedfce91bc9cc8c0e5272cd238f98e5
Hyp4r <fkatholm@gmail.com>
2022-10-08 13:46:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,29 +27,3 @@ client.on('interactionCreate', async interaction => {
 
 // Login to Discord with your client's token
 client.login(token);
-
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  {
-    name: 'Hello',
-    description: 'Replies with Hello!',
-  },
-];
-
-const rest = new rest({ version: '10' }).setToken(token);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
