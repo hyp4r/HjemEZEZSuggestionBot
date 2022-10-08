180ddfae6d205c29fa2f3fbd09d63c884369e0db
Hyp4r <fkatholm@gmail.com>
2022-10-08 16:47:44 +0200

    commands in their own folder

--- bot.js
+++ bot.js
@@ -1,11 +1,8 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits, InteractionResponse } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -15,22 +12,11 @@ client.on('interactionCreate', async interaction => {
 
 	const { commandName } = interaction;
 
-
-  switch(commandName) {
-    case 'ping':
-      await interaction.reply('Pong!');
-      break;
-    case 'server':
-      await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreation Date: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
-      break;
-    case 'user':
-      await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-      break;
-    case 'hello':
-      await interaction.reply('Hello!');
-      break;
-};
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
 });
 
-// Login to Discord with your client's token
 client.login(token);
