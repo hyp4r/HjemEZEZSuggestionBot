dca917ddd0f0d71e5013c40a8b1f202cc7b3b43e
Hyp4r <fkatholm@gmail.com>
2022-10-08 13:58:03 +0200

    Update bot.js
    fixed /server command

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
-		await interaction.reply('Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}');
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
 		await interaction.reply('User info.');
 	} else if (commandName === 'hello') {
