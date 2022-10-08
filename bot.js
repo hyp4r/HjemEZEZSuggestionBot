1bfd5002fbc50b3e852d81860cf378faddc05343
Hyp4r <fkatholm@gmail.com>
2022-10-08 14:00:38 +0200

    Update bot.js
    added /user command
    added creationDate and verificationLevel  to /server command

--- bot.js
+++ bot.js
@@ -18,9 +18,9 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreation Date: ${interaction.guild.createdAt}\nVerification level: ${interaction.guild.verificationLevel}`);
 	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	} else if (commandName === 'hello') {
     await interaction.reply('Hello!');
   }
